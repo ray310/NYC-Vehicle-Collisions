daac7022ed2d561b23a4288ff57c67c7cfd74ac0
ray310 <64942339+ray310@users.noreply.github.com>
2024-06-12 15:46:56 -0700

    Add new constants

--- src/constants.py
+++ src/constants.py
@@ -24,3 +24,10 @@ MONTHS_MAP = {
     11: "Nov",
     12: "Dec",
 }
+
+SEASONS = ("Winter", "Spring", "Summer", "Fall")
+
+NYC_WEST_LIMIT = -74.30  # west of Staten Island
+NYC_EAST_LIMIT = -73.70  # east of Queens / Lakeville Rd.
+NYC_SOUTH_LIMIT = 40.45  # south of Staten Island
+NYC_NORTH_LIMIT = 40.95  # north of the Bronx
